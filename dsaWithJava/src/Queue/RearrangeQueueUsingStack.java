dba0540ba204d1b17ef3c179bc87830ac0b8eaeb
Ujjaval Saini <sainiujvl@gmail.com>
2026-01-20 18:04:37 +0530

    Simplify loop syntax in RearrangeQueueUsingStack for better readability.

--- dsaWithJava/src/Queue/RearrangeQueueUsingStack.java
+++ dsaWithJava/src/Queue/RearrangeQueueUsingStack.java
@@ -17,19 +17,13 @@ public class RearrangeQueueUsingStack
 
         // first half elements (1 2 3 4) stack me daal diye
         Stack<Integer> st = new Stack<>();
-        for(int i = 0; i<n/2; i++){
-            st.push(q.remove());
-        }
+        for(int i = 0; i<n/2; i++)  st.push(q.remove());
 
         // add stack all elements in the queue  5 6 7 8 4 3 2 1
-        while(!st.isEmpty()){
-            q.add(st.pop());
-        }
+        while(!st.isEmpty())  q.add(st.pop());
 
         // again first helf elemets (5 6 7 8) statck me daal diye
-        for(int i = 0; i<n/2; i++){
-            st.push(q.remove());
-        }
+        for(int i = 0; i<n/2; i++)  st.push(q.remove());
 
         // jab tk stack khali nhi ho jaata tab tk ek element stack ka aur ek element queue ka remove and add karte raho
         while(!st.isEmpty()){
@@ -38,14 +32,10 @@ public class RearrangeQueueUsingStack
         }
 
         // ab order shi krne k liye saare elements stack me daal do
-        while(q.size()>0){
-            st.push(q.remove());
-        }
+        while(q.size()>0)  st.push(q.remove());
 
         // rearranged order k liye stack k saare elements queue me daal do
-        while(st.size()>0){
-            q.add(st.pop());
-        }
+        while(st.size()>0)  q.add(st.pop());
         return q;
     }
 
