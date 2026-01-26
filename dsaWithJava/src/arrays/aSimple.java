e666f23b17f4e3497328502347700ada690a49fd
Ujjaval Saini <sainiujvl@gmail.com>
2026-01-26 23:36:43 +0530

    Add isAnagram method to check if two strings are anagrams.

--- dsaWithJava/src/arrays/aSimple.java
+++ dsaWithJava/src/arrays/aSimple.java
@@ -1,8 +1,25 @@
 package arrays;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+
 public class aSimple {
-    static void main() {
-        int[] arr = {1,2,3,4,5,6};
-        System.out.println(arr.length);
+    public static void main(String[] args) {
+
+    }
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        HashSet<Character> hs1 = new HashSet<>();
+        for(int i = 0; i<s.length(); i++){
+            hs1.add(s.charAt(i));
+        }
+        HashSet<Character> hs2 = new HashSet<>();
+        for(int j = 0; j<t.length(); j++){
+            hs2.add(t.charAt(j));
+        }
+
+        return hs1.equals(hs2);
+
+
     }
 }
