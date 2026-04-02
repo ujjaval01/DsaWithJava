57d5abf12299428dbb4b23adb7205beb56caba76
Ujjaval Saini <sainiujvl@gmail.com>
2026-04-02 11:58:42 +0530

    Add NormalNumberPrinting class to display numbers in sequence.

---
+++ dsaWithJava/src/loops/NormalNumberPrinting.java
@@ -0,0 +1,13 @@
+package loops;
+
+public class NormalNumberPrinting {
+    public static void main(String[] args) {
+        int n = 5;
+        int a = n;
+        for(int i = 1; i<=n; i++){
+            System.out.println(i);
+            System.out.println(a);
+            a--;
+        }
+    }
+}
