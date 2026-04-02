8a3a34bca9d42945d04f2c1941864687b6dfac88
Ujjaval Saini <sainiujvl@gmail.com>
2026-04-02 11:58:28 +0530

    Update main method signature and optimize loop for clarity.

--- dsaWithJava/src/SortBubble/MoveAllZerosInTheEnd.java
+++ dsaWithJava/src/SortBubble/MoveAllZerosInTheEnd.java
@@ -1,12 +1,13 @@
 package SortBubble;
 
 public class MoveAllZerosInTheEnd {
-    static void main() {
+    public static void main(String[] args) {
         int[] arr = {3,5,0,0,4};
-
+        int n = arr.length;
         int j = 0;
-        for(int i = 0; i < arr.length; i++){
-           if(arr[i] != 0){
+
+        for(int i = 0; i<n; i++){
+            if(arr[i] !=0 ){
                 int temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
