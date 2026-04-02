370419e53f4f023ee79bc5b44d563541392881d3
Ujjaval Saini <sainiujvl@gmail.com>
2026-04-02 11:58:06 +0530

    Refactor the main method and optimize sorting logic for clarity.

--- dsaWithJava/src/SortBubble/BubbleSortOptimized.java
+++ dsaWithJava/src/SortBubble/BubbleSortOptimized.java
@@ -4,7 +4,7 @@ Best case T.C - O(n)
 Avg & worst case T.C- O(n*n)
 */
 public class BubbleSortOptimized {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         int[] arr = {1,2,5,3,4};
         int n = arr.length;
         sortArray(arr,n);
@@ -13,42 +13,37 @@ public class BubbleSortOptimized {
         }
     }
 
-
     static void sortArray(int[] arr, int n){
-
-       for(int i = 0; i< n-1; i++){
-           boolean flag = false;
-           for(int j = 0; j< n-i-1; j++){
-               if(arr[j] > arr[j+1]){
-                   int temp = arr[j];
-                   arr[j] = arr[j+1];
-                   arr[j+1] = temp;
-                   flag = true;
-               }
-           }
-           if(!flag){
-               break;
-           }
-       }
-
+        for(int i = 0; i<n; i++){
+            boolean flag = false;
+            for (int j = 0; j<n-1-i; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    flag = true;
+                }
+            }
+            if (!flag) return;
+        }
     }
 }
 /*
 Pass 1 (i = 0)
-i	j	Compare	Swap?	Array State	flag
-0	0	1 > 2	No	[1, 2, 5, 3, 4]	false
-0	1	2 > 5	No	[1, 2, 5, 3, 4]	false
-0	2	5 > 3	Yes	[1, 2, 3, 5, 4]	true
-0	3	5 > 4	Yes	[1, 2, 3, 4, 5]	true
+i	j	Compare	Swap?	Array State	    flag
+0	0	1 > 2	 No	    [1, 2, 5, 3, 4]	false
+0	1	2 > 5	 No	    [1, 2, 5, 3, 4]	false
+0	2	5 > 3	Yes	    [1, 2, 3, 5, 4]	true
+0	3	5 > 4	Yes	    [1, 2, 3, 4, 5]	true
 
 👉 Pass 1 ke baad array sorted ho gaya, flag = true.
 
 Pass 2 (i = 1)
-i	j	Compare	Swap?	Array State	flag
-1	0	1 > 2	No	[1, 2, 3, 4, 5]	false
-1	1	2 > 3	No	[1, 2, 3, 4, 5]	false
-1	2	3 > 4	No	[1, 2, 3, 4, 5]	false
+i	j	Compare	Swap?	Array State	    flag
+1	0	1 > 2	No	    [1, 2, 3, 4, 5]	false
+1	1	2 > 3	No	    [1, 2, 3, 4, 5]	false
+1	2	3 > 4	No	    [1, 2, 3, 4, 5]	false
 
-👉 Is pass me koi swap nahi hua, flag = false.
+👉 Is pass me koi swap nhi hua, flag = false.
 👉 Condition if(!flag) break; → loop break ho jaayega.
 */
