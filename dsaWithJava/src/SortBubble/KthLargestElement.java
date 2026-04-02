971cc6145edd19458c93701a3417964170e780c0
Ujjaval Saini <sainiujvl@gmail.com>
2026-04-02 11:58:16 +0530

    Update main method signature and optimize array length usage.

--- dsaWithJava/src/SortBubble/KthLargestElement.java
+++ dsaWithJava/src/SortBubble/KthLargestElement.java
@@ -1,18 +1,18 @@
 package SortBubble;
 
 public class KthLargestElement {
-    static void main() {
+    public static void main(String[] args) {
         int[] arr = {7, 10, 4, 3, 20, 15};
         // 3 4 7 10 15 20
         int k = 2;
         int res = findElement(arr,k);
         System.out.println(res);
-
     }
 
     private static int findElement(int[] arr, int k) {
+        int n = arr.length;
         for(int i = 0; i<k; i++){
-            for(int j = 0; j< arr.length-i-1; j++){
+            for (int j = 0; j<n-1-i; j++){
                 if(arr[j] > arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
