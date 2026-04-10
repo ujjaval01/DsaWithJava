285094756d6a3af908f33e2c9cc980283d63023c
Ujjaval Saini <sainiujvl@gmail.com>
2026-04-10 15:04:24 +0530

    Update main method to demonstrate string modification instead of case change.

--- dsaWithJava/src/Revision/ChangeTheString.java
+++ dsaWithJava/src/Revision/ChangeTheString.java
@@ -3,7 +3,13 @@ package Revision;
 public class ChangeTheString {
     public static void main(String[] args) {
         String s = "Abcd";
-        System.out.println(modify(s));
+
+        // modifying the string(Like spelling correction)
+        String a = "ujjaval";
+        a = a.substring(0, 4) + 'b' + a.substring(5,7);
+        System.out.println(a);
+
+//        System.out.println(modify(s));
     }
     public static String modify(String s) {
         int a = (int)s.charAt(0);
