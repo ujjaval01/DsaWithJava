3b963e2f837ee8eb46e8e9090b01eb1321a527e4
Ujjaval Saini <sainiujvl@gmail.com>
2026-05-06 15:46:51 +0530

    Refactor fabonaciSeries.java for improved Fibonacci calculation logic.

--- dsaWithJava/src/loops/fabonaciSeries.java
+++ dsaWithJava/src/loops/fabonaciSeries.java
@@ -4,29 +4,28 @@ import java.util.Scanner;
 
  public class fabonaciSeries {
 
-//     static int fab(int n){
-//         if(n == 1 || n == 2){
-//             return n-1;
-//         }
-//         else{
-//             return fab(n-1) + fab(n-2);
-//         }
-//     }
+     static int fab(int n){
+         if(n == 1 || n == 2)
+             return n-1;
+         else{
+             return fab(n-1) + fab(n-2);
+         }
+     }
 
      public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
-        // int n = sc.nextInt();
-        // int result = fab(n);
-        // System.out.println(result);
-        int range = sc.nextInt();
-        int first = 0; int second = 1; int next;
-        System.out.print("The series is: ");
-        for(int i = 0; i<=range; i++){
-            System.out.print(first + ", ");
-            next = first + second;
-            first = second;
-            second = next;
-        }
+         int n = sc.nextInt();
+         int result = fab(n);
+         System.out.println(result);
+//        int range = sc.nextInt();
+//        int first = 0, second = 1, next;
+//        for(int i = 0; i<=range; i++){
+//            System.out.print(first + ", ");
+//            next = first+second;
+//            first = second;
+//            second = next;
+//
+//        }
      }
 }
