4c2a2ed8f5756cc21a4a83a2dfca46afb5092814
Ujjaval Saini <sainiujvl@gmail.com>
2026-05-06 15:45:24 +0530

    Refactor main method for improved clarity in binary search logic.

--- dsaWithJava/src/SearchBinary/BinarySearching.java
+++ dsaWithJava/src/SearchBinary/BinarySearching.java
@@ -19,7 +19,7 @@ Explanation: 2 does not exist in nums so return -1
 */
 
 public class BinarySearching {
-    static void main() {
+    public static void main(String[] args) {
         int[] arr = {-4,2,4,5,8,9,14,17};
         int target = 14;
         int res = search(arr,target);
@@ -27,13 +27,23 @@ public class BinarySearching {
     }
 
     public static int search(int[] arr, int target){
+//        int low = 0;
+//        int high = arr.length-1;
+//        while(low<=high){
+//            int mid = (low + high)/2;
+//            if(arr[mid] == target) return mid;
+//            else if (arr[mid] < target) low = mid + 1;
+//            else high = mid-1;
+//        }
+//        return -1;
+
         int low = 0;
         int high = arr.length-1;
-        while(low<=high){
-            int mid = (low + high)/2;
-            if(arr[mid] == target) return mid;
-            else if (arr[mid] < target) low = mid + 1;
-            else high = mid-1;
+        while (low <= high){
+            int mid = (high + low)/2;
+            if (arr[mid] == target) return mid;
+            else if(target > arr[mid]) low = mid+1;
+            else high = mid -1;
         }
         return -1;
     }
