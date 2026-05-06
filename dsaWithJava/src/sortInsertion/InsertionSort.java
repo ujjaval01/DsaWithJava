59f9fe35e7479df9e2ea5d22ad6a9844c3f65376
Ujjaval Saini <sainiujvl@gmail.com>
2026-05-06 15:47:22 +0530

    Refactor InsertionSort.java to make main method public and visible.

--- dsaWithJava/src/sortInsertion/InsertionSort.java
+++ dsaWithJava/src/sortInsertion/InsertionSort.java
@@ -1,10 +1,11 @@
 package sortInsertion;
 
 public class InsertionSort {
-    static void main() {
+    public static void main(String[] args) {
         int[] arr = {12,11,13,5,6};
-        for(int i = 1; i< arr.length; i++){
-            int j = i;
+
+        for(int i = 1; i<arr.length; i++){
+            int j = i;   
             while (j > 0 && arr[j] < arr[j-1]){
                 int temp = arr[j];
                 arr[j] = arr[j-1];
