1017b96bcc00c90619610f030d8e7ea6f561df4d
Ujjaval Saini <sainiujvl@gmail.com>
2026-05-06 15:45:39 +0530

    Refactor ArmstrongCheck.java for clarity in digit sum calculation.

--- dsaWithJava/src/loops/ArmstrongCheck.java
+++ dsaWithJava/src/loops/ArmstrongCheck.java
@@ -5,22 +5,27 @@ class ArmstrongCheck {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the Number: ");
-        int num = sc.nextInt();
-        int n = num;
+        int num = sc.nextInt();     // 1 5 3
         int sum = 0;
         int digit = 0;
 
-        while (num != 0) {        // checking the digit 
-            num = num / 10;
+        int temp = num;
+        while (temp != 0) {        // checking the digit
+            temp = temp / 10;
             digit++;
         }
-       
         // calculating the sum of the digit ^ digit
-         while (n != 0) {
-            digit = n % 10;
-            
-
+         temp = num;
+         while (temp != 0) {
+             int a = temp%10;
+             sum = sum + (int) Math.pow(a, digit);
+             temp = temp/10;
          }
+
+//        System.out.println(sum);
+
+         if(num == sum) System.out.println("Armstrong");
+         else System.out.println("Not a armstrong");
 
 
     }
