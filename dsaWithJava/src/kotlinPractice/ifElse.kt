80e754ad549454287a2b8330761e514d4b07f737
Ujjaval Saini <sainiujvl@gmail.com>
2026-05-07 22:20:41 +0530

    Add ifElse.kt to implement simple age-based conditional logic.

---
+++ dsaWithJava/src/kotlinPractice/ifElse.kt
@@ -0,0 +1,11 @@
+package kotlinPractice
+
+fun main(){
+    var age = 19
+
+//    if(age >= 18) println("elder") else println("Lower")
+
+    var res = if(age >= 18) "elder" else "Lower"
+    println(res)
+
+}
