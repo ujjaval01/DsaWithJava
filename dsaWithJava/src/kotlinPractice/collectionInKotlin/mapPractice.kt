ec362b788d3f45b3764aa5d65437a91266daa627
ujjaval01 <sainiujvl@gmail.com>
2026-07-21 23:50:09 +0530

    Add mapPractice.kt to implement mutable map and print functionality.

---
+++ dsaWithJava/src/kotlinPractice/collectionInKotlin/mapPractice.kt
@@ -0,0 +1,6 @@
+package kotlinPractice.collectionInKotlin
+
+fun main(){
+    val map = mutableMapOf<Int, String>(1 to "Ujval", 2 to "zana", 2 to "saini")  // print with unique key
+    println(map)
+}
