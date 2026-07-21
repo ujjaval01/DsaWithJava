2abf3acbb31aae5569703d54ef70a2f69131f01a
ujjaval01 <sainiujvl@gmail.com>
2026-07-21 23:49:47 +0530

    Refactor Main.java to enhance class structure and clarity.

--- dsaWithJava/src/Main.java
+++ dsaWithJava/src/Main.java
@@ -1,6 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args) {
         System.out.println("hello");
+
+        System.out.println((6%2 == 0) ? true : false);
+
+
 
 //        int p = 67, t = 2, r = 5;
 //        double si = p*t*r/100d;
@@ -16,7 +25,7 @@ public class Main{
 //            System.out.println();
 //        }
 
-        int n = 4;
+//        int n = 4;
 //        * * * *
 //        * * *
 //        * *
@@ -29,13 +38,32 @@ public class Main{
 //        }
 
         //product of the array elements
-        int[] a = {2,4,5,3,6}, b = {2,4,5,3,6}, c = new int[a.length]; int p = 1;
-        for(int i = 0; i<a.length; i++) c[i] = a[i]*b[i];
-        for(int res: c) System.out.print(res + " ");
+//        int[] a = {2,4,5,3,6}, b = {2,4,5,3,6}, c = new int[a.length]; int p = 1;
+//        for(int i = 0; i<a.length; i++) c[i] = a[i]*b[i];
+//        for(int res: c) System.out.print(res + " ");
 
 
+        // Ashish question
 
-
+//        Scanner sc = new Scanner(System.in);
+//        Random ran = new Random();
+//        String[] arr = {"ujvl", "Ashish","aman","sorav","ajay","summit","shivam","hrsit","summit","amit"};
+//        ArrayList<String> al = new ArrayList<>(List.of(arr));
+//
+//        System.out.print("Enter a number: ");
+//        int n = sc.nextInt();                     // 4
+//        int r = ran.nextInt(0, 10);  // 5
+//
+//        if(n == r){
+//            System.out.println(al.get(n-1));
+//            System.out.println(al.get(n-1).length());
+//        }
+//        else{
+//            System.out.print("Enter a name: ");
+//            String name = sc.next();
+//            al.add(name);
+//        }
+//
 
     }
 }
